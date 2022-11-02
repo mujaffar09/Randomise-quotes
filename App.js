62541f99a7fc5d652397bbf3188305627ebc367c
mujaffar09 <110367256+mujaffar09@users.noreply.github.com>
2022-11-03 00:40:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Randomiser from './components/Randomiser';
+
+function App() {
+  return (
+    <div className="App">
+       <Randomiser />
+    </div>
+  );
+}
+
+export default App;
